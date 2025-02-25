c13a6e6bf2b831ee01aa4c9aeb88f47b762d26e3
wenxuan <deltaleung3@gmail.com>
2025-02-25 21:35:35 +0800

    lab2-part1

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,30 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  uint32_t n32 = n % ( 1UL << 32 );
+  return zero_point + n32;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t pow232 = ( 1UL << 32 );
+  uint64_t seq_num = ( this->raw_value_ - zero_point.raw_value_ ) % pow232;
+  uint64_t order = checkpoint / pow232;
+
+  uint64_t result1 = seq_num + order * pow232;
+  uint64_t result2 = result1 + pow232;
+  uint64_t result0 = ( result1 >= pow232 ) ? ( result1 - pow232 ) : ( result1 + pow232 );
+
+  uint64_t diff1 = ( result1 > checkpoint ) ? ( result1 - checkpoint ) : ( checkpoint - result1 );
+  uint64_t diff2 = ( result2 > checkpoint ) ? ( result2 - checkpoint ) : ( checkpoint - result2 );
+  uint64_t diff0 = ( result0 > checkpoint ) ? ( result0 - checkpoint ) : ( checkpoint - result0 );
+
+  // 返回差异最小的值对应的 result
+  if ( diff0 <= diff1 && diff0 <= diff2 ) {
+    return result0;
+  } else if ( diff1 <= diff2 ) {
+    return result1;
+  } else {
+    return result2;
+  }
 }
